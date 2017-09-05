b24e1ec1d933c7b982b1a25ef3752b5d9c4bbde9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-05 15:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 function theBeatlesPlay(musicians, instruments) {
- var newArray = []
-  for (var i = 0; i < musicians.length; i++)
-  array.push(`${musicians[i]} plays ${instruments[i]}`)
+   var array = [];
+ 
+   for (var i = 0; i<4; i++) {
+     array.push(`${musicians[i]} plays ${instruments[i]}`)
+   }
   return array;
-
-}
-function johnLennonFacts(facts) {
-  var exclamedfacts = [];
-   let n = 0;
+ }
+ 
+ function johnLennonFacts(facts) {
+ 
+   var exclamedfacts = [];
+  let n = 0;
    while ( n < facts.length ) {
-   exclamedfacts.push (facts[n] + "!!!");
-   n++;
+     exclamedfacts.push (facts[n] + "!!!");
+     n++;
    }
    return exclamedfacts;
-}
-
-function iLoveTheBeatles(n) {
-  var beatles = [];
-  do {beatles.push("I love the Beatles!");}
-  n++;
-while (n < 15);
-return beatles
-}
+ }
+ 
+ function iLoveTheBeatles(x) {
+   var lovebeatles = [];
+   do {
+     lovebeatles.push("I love the Beatles!")
+     x++;
+   } while (x < 15);
+   return lovebeatles;
+ }
